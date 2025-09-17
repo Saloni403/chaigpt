1be6cecc92d06c418c931696b5cf7839c2b8ce54
Saloni403 <156641454+Saloni403@users.noreply.github.com>
2025-09-17 18:08:51 +0530

    vvv

--- web/js/indexservices.js
+++ web/js/indexservices.js
@@ -1,13 +1,26 @@
- let buttons = document.querySelectorAll(".service-btn");
-      let servicediv = document.querySelectorAll(".service-div");
-      buttons.forEach((button) => {
-        servicediv[1].style.display = "none";
-        servicediv[2].style.display = "none";
-        button.addEventListener("click", () => {
-          servicediv.forEach((div) => {
-            div.style.display = "none";
-          });
-          let target = button.getAttribute("data-target");
-          document.querySelector("." + target).style.display = "flex";
-        });
-      });
+//  let buttons = document.querySelectorAll(".service-btn");
+//       let servicediv = document.querySelectorAll(".service-div");
+//       buttons.forEach((button) => {
+//         servicediv[1].style.display = "none";
+//         servicediv[2].style.display = "none";
+//         button.addEventListener("click", () => {
+//           servicediv.forEach((div) => {
+//             div.style.display = "none";
+//           });
+//           let target = button.getAttribute("data-target");
+//           document.querySelector("." + target).style.display = "flex";
+//         });
+//       });
+
+
+
+
+
+ const video = document.getElementById("bgVideo");
+  const unmuteBtn = document.getElementById("unmuteBtn");
+
+  unmuteBtn.addEventListener("click", () => {
+    video.muted = false;   // unmute
+    video.play();          // ensure it continues playing
+    unmuteBtn.style.display = "none"; // hide button after enabling sound
+  });
